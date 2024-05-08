d32cfcbf66c3fc921083bfb61c122d9cabff2a41
gabrielesilv <gabimonteiro2012@gmail.com>
2024-05-07 21:38:14 -0300

    alterações pelo cmd

---
+++ java-terminal/src/MinhaClasse.java
@@ -0,0 +1,12 @@
+//Executando código fora da IDE (pelo prompt de comando)
+
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("Oi pessoal, fui executado pelo Terminal");
+    }
+}
+
+/*entrar na pasta bin e executar o comando java + nome da classe, por exemplo: 
+
+C:\estudos\dio-trilha-java-basico\java-terminal\bin>java MinhaClasse
+*/
